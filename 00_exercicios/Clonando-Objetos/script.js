49a7e3c9f4e819810459dfb61598978ad2b805b0
SamuelLuzSantana <slsamuelluz@gmail.com>
2022-02-08 12:45:35 -0300

    continuação dos exericícios

--- 00_exercicios/Clonando-Objetos/script.js
+++ 00_exercicios/Clonando-Objetos/script.js
@@ -4,3 +4,18 @@ const celular = {
     capacidadeBateria: 5000,
     ligar: function () { console.log("Fazendo ligação...") }
 }
+
+//metodo 1 - com essa forma é possivel adicionar  
+//novos atributos no objeto clonado
+const novoObjeto = Object.assign({
+
+    cabo: true 
+
+}, celular);
+
+console.log(novoObjeto);
+
+
+// segundo metodo
+const objeto2 = {...celular};
+console.log(objeto2);
