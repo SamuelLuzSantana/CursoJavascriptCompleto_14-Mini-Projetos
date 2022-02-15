294ebef0a23fd44bbfedfb3d442efdce557afd49
SamuelLuzSantana <slsamuelluz@gmail.com>
2022-02-15 09:59:16 -0300

    exercícios 11, 12 e 13 completos

---
+++ 13_Montando-Postagem-de-Blog/script.js
@@ -0,0 +1,13 @@
+let postagem = {
+    titulo : 'dasd',
+    mensagem : 'fagfa',
+    autor : 'gafga',
+    visualizacoes: 20,
+    comentarios : [
+        {autor : 'a', mensagem: 'b'},
+        {autor : 'b', mensagem: 'd'}
+    ],
+    estaAovivo: true
+}
+
+console.log(postagem)
